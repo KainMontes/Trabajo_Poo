35034de1f69b06f8cbadb82bf518a3e8b591a2cb
Jofre Montes <Alexissaravia159@gmail.com>
2024-04-14 16:52:51 -0500

    Ojala vuelva :v

---
+++ src/main/java/Trabajo_POO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Alexis Montes
+ */
+public class Trabajo_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
